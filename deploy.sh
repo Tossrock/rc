c0577bb04f08bc761684fa9369ed9ae38600725d
Kevin Reynolds <kreynolds@quantcast.com>
2015-08-18 15:46:10 -0700

    Adding deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp vimrc ~/.vimrc
+cp zshrc ~/.zshrc
+mkdir ~/.tmux
+cp tmux-copy ~/.tmux/tmux-copy
+cp tmux.conf ~/.tmux/tmux-conf
+cp "start.py" ~/".start.py"
+mkdir -p ~/iterm2/prefs
+cp com.googlecode.iterm2.plist ~/iterm2/prefs
