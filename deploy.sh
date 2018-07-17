98e101dce91040b4f93466fc971115ec93465e4f
Kevin Reynolds <tossrock@gmail.com>
2018-07-17 13:08:13 -0700

    Fix compdef stuff for zshrc

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cp vimrc ~/.vimrc
 cp zshrc ~/.zshrc
 mkdir ~/.tmux
 cp tmux-copy ~/.tmux/tmux-copy
-cp tmux.conf ~/.tmux/tmux-conf
+cp tmux.conf ~/.tmux.conf
 cp "start.py" ~/".start.py"
 mkdir -p ~/iterm2/prefs
 cp com.googlecode.iterm2.plist ~/iterm2/prefs
